b4fd6f9b7c8f406666da3293dded37f3156d7224
codenamedroid <codenamegit@gmail.com>
2012-02-22 13:54:17 -0600

    add olympus

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,13 +4,15 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cna_crespo4g.mk \
     $(LOCAL_DIR)/cna_maguro.mk \
     $(LOCAL_DIR)/cna_toro.mk \
-    $(LOCAL_DIR)/cna_stingray.mk \
-    $(LOCAL_DIR)/cna_stingray_cdma.mk \
-    $(LOCAL_DIR)/cna_umts_everest.mk \
-    $(LOCAL_DIR)/cna_wingray.mk \
-    $(LOCAL_DIR)/cna_tf101.mk \
     $(LOCAL_DIR)/cna_galaxysmtd.mk \
     $(LOCAL_DIR)/cna_fascinatemtd.mk \
     $(LOCAL_DIR)/cna_captivatemtd.mk \
     $(LOCAL_DIR)/cna_vibrantmtd.mk \
-    $(LOCAL_DIR)/cna_inc.mk
+    $(LOCAL_DIR)/cna_inc.mk \
+    $(LOCAL_DIR)/cna_olympus.mk \
+    $(LOCAL_DIR)/cna_stingray.mk \
+    $(LOCAL_DIR)/cna_stingray_cdma.mk \
+    $(LOCAL_DIR)/cna_umts_everest.mk \
+    $(LOCAL_DIR)/cna_wingray.mk \
+    $(LOCAL_DIR)/cna_tf101.mk
+
