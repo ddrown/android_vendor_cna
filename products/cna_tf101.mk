38dc50d7034d075d7f355a1fc9296ea8e170ca75
codenamedroid <codenamegit@gmail.com>
2012-02-26 19:01:28 -0600

    updates for tf101
    Change-Id: I9232bcd0fbe5492cf0027d188be797e69606db6e

--- products/cna_tf101.mk
+++ products/cna_tf101.mk
@@ -1,13 +1,15 @@
-$(call inherit-product, device/asus/tf101/device_tf101.mk)
+$(call inherit-product, device/asus/tf101/full_tf101.mk)
 
 # Inherit some common stuff.
 $(call inherit-product, vendor/cna/config/common_full_tablet.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := EEE-PAD
+PRODUCT_RELEASE_NAME := TF-EeePad
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=US_epad BUILD_ID=IML74K BUILD_FINGERPRINT=asus/US_epad/EeePad:4.0.3/IML74K/US_epad-9.2.1.11-20120221:user/release-keys PRIVATE_BUILD_DESC="US_epad-user 4.0.3 IML74K US_epad-9.2.1.11-20120221 release-keys"
 
 PRODUCT_NAME := cna_tf101
 PRODUCT_DEVICE := tf101
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := Transformer TF101
-PRODUCT_MANUFACTURER := Asus
+PRODUCT_MODEL := Transformer
+PRODUCT_MANUFACTURER := asus
