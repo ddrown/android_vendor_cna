863a4fa21130b6e97a2ccd45d1e68695d4573f11
Codename Android <codenamedroid@codenameandroid.com>
2012-03-20 16:16:19 -0500

    don't make these when installing gapps
    calendar email and exchange now in gapps
    
    Change-Id: Id717320bf384b190cd964b89bd791f88b4c45e8b

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 # We need a way to prevent the stuff Google Apps replaces from being included in the build.
 # This is a hacky way to do that.
 ifdef WITH_GOOGLE_APPS
-    PACKAGES.Phone.OVERRIDES := Gallery2 QuickSearchBox PicoTts Provision
+    PACKAGES.Phone.OVERRIDES := Gallery2 QuickSearchBox PicoTts Provision Calendar Email Exchange
 endif
