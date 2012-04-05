7ed79fd8c57483b7db9485c750c3a25ea3c275cb
Codename Android <codenamedroid@codenameandroid.com>
2012-04-05 01:30:43 -0500

    add htc phone (inc and vivow)

---
+++ products/cna_mod_vivow.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/htc/vivow/full_vivow.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/cna/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/cna/config/gsm.mk)
+
+PRODUCT_COPY_FILES +=  \
+    vendor/cna/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# Release name
+PRODUCT_RELEASE_NAME := Inc2
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=verizon_wwe/htc_vivow/vivow:2.3.4/GRJ22/120826.3:user/release-keys PRIVATE_BUILD_DESC="4.08.605.3 CL120826 release-keys"
+
+PRODUCT_NAME := cna_mod_vivow
+PRODUCT_DEVICE := vivow
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,10 +6,12 @@ add_lunch_combo cna_mod_fascinatemtd-userdebug
 add_lunch_combo cna_mod_galaxys2-userdebug
 add_lunch_combo cna_mod_galaxysmtd-userdebug
 add_lunch_combo cna_mod_i777-userdebug
+add_lunch_combo cna_mod_inc-userdebug
 add_lunch_combo cna_mod_stingray-userdebug
 add_lunch_combo cna_mod_tf101-userdebug
 add_lunch_combo cna_mod_tf201-userdebug
 add_lunch_combo cna_mod_toro-userdebug
 add_lunch_combo cna_mod_vibrantmtd-userdebug
+add_lunch_combo cna_mod_vivow-userdebug
 add_lunch_combo cna_mod_wingray-userdebug
 
